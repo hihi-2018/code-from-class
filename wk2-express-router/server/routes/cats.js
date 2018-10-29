16d79739f3973dea749d8c99e44625fc4969413f
Ross Jourdain <>
2018-10-29 17:40:30 +1300

    Express files

--- wk2-express-router/server/routes/cats.js
+++ wk2-express-router/server/routes/cats.js
@@ -2,7 +2,20 @@ let express = require('express')
 let router = express.Router()
 
 router.get('/', (req, res) => {
-  res.send('This is the cats page')
+  // Send a string
+  res.send('<h1>This is the cats page</h1>')
+
+  // Send a file
+  // res.sendFile('index.html')
+
+  // Render and send to client a template (from the view folder) using provided data
+  // res.render('the name of my template', { my_data_object: 'yes!' })
+
+  // Set the status code and send
+  // res.sendStatus(404)
+
+  // Set the status but don't send
+  // res.status
 })
 
 router.get('/:id', (req, res) => {
--- wk2-express-router/server/routes/dogs.js
+++ wk2-express-router/server/routes/dogs.js
@@ -1,8 +1,24 @@
 let express = require('express')
 let router = express.Router()
+let fs = require('fs')
+
+let puppyDataNoTwo = require('../../data.json')
 
 router.get('/', (req, res) => {
-  res.send('This is the dogs page')
+  fs.readFile('./data.json', 'utf8', (err, data) => {
+    if (err) {
+      console.log('Errror', err)
+      res.send('There was an error')
+    } else {
+      // console.log('data', typeof data)
+      let puppyData = JSON.parse(data)
+      // console.log('puppyData', typeof puppyData)
+      console.log('puppyData ', puppyData)
+      console.log('puppyData ', JSON.stringify(puppyData, null, 4))
+      console.log('stringify ', JSON.stringify(puppyData))
+      res.send('OK')
+    }
+  })
 })
 
 module.exports = router
